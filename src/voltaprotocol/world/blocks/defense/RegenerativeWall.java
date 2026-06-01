53138cb16be43e26f07bb378d1114bcc7bf014de
LeinadHQ <daniellolcocycr@gmail.com>
2026-06-01 11:19:32 -0400

    Update RegenerativeWall.java

--- src/voltaprotocol/world/blocks/defense/RegenerativeWall.java
+++ src/voltaprotocol/world/blocks/defense/RegenerativeWall.java
@@ -1,5 +1,9 @@
 package voltaprotocol.world.blocks.defense;
 
+import arc.graphics.Color;
+import mindustry.content.Fx;
+import mindustry.entities.Effect;
+import mindustry.entities.effect.ParticleEffect;
 import mindustry.world.blocks.defense.Wall;
 
 public class RegenerativeWall extends Wall {
@@ -8,6 +12,16 @@ public class RegenerativeWall extends Wall {
     public float healInterval = 30f;
     public float regenDelay = 180f;
     public float maxHeal = 10f;
+
+    public Effect successHealEffect = new ParticleEffect() {{
+        particles = 3;
+        lifetime = 15f;
+        length = 8f;
+        sizeFrom = 1f;
+        sizeTo = 0f;
+        colorFrom = Color.valueOf("84f5f5");
+        colorTo = Color.valueOf("5eb1b1");
+    }};
 
     public RegenerativeWall(String name, int size, int health, float armor, float healAmount, float maxHeal) {
         super(name);
@@ -42,6 +56,7 @@ public class RegenerativeWall extends Wall {
     public class RegenerativeWallBuild extends WallBuild {
         private float healTimer = 0f;
         private float damageTimer = 0f;
+        private boolean regenerationActive = false;
 
         @Override
         public void updateTile() {
@@ -49,18 +64,39 @@ public class RegenerativeWall extends Wall {
 
             if(damageTimer > 0f){
                 damageTimer -= edelta();
+
+                regenerationActive = false;
                 return;
             }
 
             if(health < maxHealth){
+
+                if(!regenerationActive){
+                    regenerationActive = true;
+
+                    successHealEffect.at(x, y);
+                    Fx.healBlock.at(x, y, block.size);
+                }
+
                 healTimer += edelta();
+
                 if(healTimer >= healInterval){
                     healTimer = 0f;
-                    float missingPercent = (maxHealth - health) / maxHealth;
-                    float healValue = healAmount + healAmount * missingPercent * 2f;
+
+                    float missingPercent =
+                        (maxHealth - health) / maxHealth;
+
+                    float healValue =
+                        healAmount +
+                        healAmount * missingPercent * 2f;
+
                     healValue = Math.min(healValue, maxHeal);
+
                     heal(healValue);
                 }
+
+            }else{
+                regenerationActive = false;
             }
         }
 
@@ -69,6 +105,7 @@ public class RegenerativeWall extends Wall {
             super.damage(damage);
             damageTimer = regenDelay;
             healTimer = 0f;
+            regenerationActive = false;
         }
     }
 }
