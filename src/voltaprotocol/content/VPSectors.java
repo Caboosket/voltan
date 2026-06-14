14162731ddf4439bd8b925ea80be4ccbd2f8b863
LeinadHQ <daniellolcocycr@gmail.com>
2026-06-14 00:27:17 -0400

    Update VPSectors.java

--- src/voltaprotocol/content/VPSectors.java
+++ src/voltaprotocol/content/VPSectors.java
@@ -1,16 +1,30 @@
 package voltaprotocol.content;
 
+import mindustry.content.Items;
+import mindustry.type.ItemStack;
 import mindustry.type.SectorPreset;
+import arc.struct.Seq;
 
 public class VPSectors {
     public static SectorPreset laUltimaSenal;
 
     public static void load() {
         laUltimaSenal = new SectorPreset("la-ultima-senal", VPPlanets.Volta, 0) {{
-            localizedName = "La Última Señal";
             difficulty = 3;
             alwaysUnlocked = true;
-            captureWave = 40;
-        }};
+            captureWave = 40;                
+        }}; 
+
+        laUltimaSenal.rules = r -> {
+            r.bannedBlocks.clear();
+            r.damageExplosions = true;
+        r.loadout = Seq.with(ItemStack.with(
+            Items.copper, 2000,
+            Items.lead, 2000,
+            Items.silicon, 1000,
+            Items.graphite, 800,
+            VPItems.silver, 500
+        ));
+        };
     }
 }
